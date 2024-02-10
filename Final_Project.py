d5dad94f84af87c65eee2be31af00a94efc566ad
doktorkot <153201135+doktorkot@users.noreply.github.com>
2024-02-10 19:42:46 +0200

    Final_Project.py

--- Final_Project.py
+++ Final_Project.py
@@ -3,6 +3,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import requests
 from bs4 import BeautifulSoup
 import datetime
+import random
 listName = []
 listDes = []
 listHref = []
@@ -26,10 +27,17 @@ def parser():
             soup_list_ul = soup1.find_all('ul', {'class': 'short-list fx-1'})
             for i in soup_list_ul:
                 listDes.append(i.text)
+async def help(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    comands = "hello, film, searchFilm, filmInfo, filmByNumber, now"
+    await update.message.reply_text(f'це команди бота {comands}')
 
+async def randomiser(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    random = randint(1, 100)
+    await update.message.reply_text(f'ваше рандомне число {random}')
 
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+    await update.message.reply_text(f'Hello {update.effective_user.full_name}')
+    
 async def now(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     now = datetime.datetime.now()
     await update.message.reply_text(f'{now}')
@@ -38,8 +46,16 @@ async def film(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     parser()
     for film in range(len(listName)):
         await update.message.reply_text(f'{listName[film]} \n{listDes[film]}\n{listHref[film]}')
+        
+async def filmByNumber(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    number = int(update.message.text.split(" ")[1]) - 1
+    parser()
+    if 0 <= number < len(listName):
+        await update.message.reply_text(f'{listName[number]} \n{listDes[number]}\n{listHref[number]}')
+    else:
+        await update.message.reply_text(f'Фільм з номером {number + 1} не знайдено.')
 
-async def search_film(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def searchFilm(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     parser()
     search_query = update.message.text.split(" ")[1]
     for film in range(len(listName)):
@@ -49,7 +65,7 @@ async def search_film(update: Update, context: ContextTypes.DEFAULT_TYPE) -> Non
     else:
         await update.message.reply_text(f'Фільм з назвою "{search_query}" не знайдено.')
 
-async def film_info(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def filmInfo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     url = update.message.text.split(" ")[1]
     requst = requests.get(url)
     soup = BeautifulSoup(requst.text, features="html.parser")
@@ -61,18 +77,15 @@ async def film_info(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         film_des = i.text
     await update.message.reply_text(f'{film_name} \n{film_des}\n{url}')
 
-async def film_by_number(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    number = int(update.message.text.split(" ")[1]) - 1
-    parser()
-    if 0 <= number < len(listName):
-        await update.message.reply_text(f'{listName[number]} \n{listDes[number]}\n{listHref[number]}')
-    else:
-        await update.message.reply_text(f'Фільм з номером {number + 1} не знайдено.')
-
-app = ApplicationBuilder().token("6679336615:AAEuXY2yTF88SU0fcZ-1Ve8imw7T4TYK5Bg").build()
+app = ApplicationBuilder().token("5131642226:AAE89c94uSifu9_J3PlBOhC4Jk9Jy5xRwzo").build()
 
 app.add_handler(CommandHandler("hello", hello))
 app.add_handler(CommandHandler("now", now))
 app.add_handler(CommandHandler("film", film))
+app.add_handler(CommandHandler("filmByNumber", filmByNumber))
+app.add_handler(CommandHandler("filmInfo", filmInfo))
+app.add_handler(CommandHandler("searchFilm", searchFilm))
+app.add_handler(CommandHandler("help", help))
+app.add_handler(CommandHandler("random", randomiser))
 
 app.run_polling()
